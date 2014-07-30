f1f8b330d9d67f536ec48fca36675fd8aa3257bd
kacperharasim <kacper.harasim@gmail.com>
2014-07-30 02:58:16 +0200

    poprawka kodu ip validation/zmiana struktury

--- re/ip_validate.py
+++ re/ip_validate.py
@@ -1,17 +1,24 @@
 import re
-for _ in (input()):
-    str = raw_input()
+
+def validate(str):
     matchip4 = re.search(r'(\d+)[.]{1}(\d+)[.]{1}(\d+)[.]{1}(\d+)', str)
     if matchip4:
-        a = matchip4.group(1)
-        b = matchip4.group(2)
-        c = matchip4.group(3)
-        d = matchip4.group(4)
+        a = int(matchip4.group(1))
+        b = int(matchip4.group(2))
+        c = int(matchip4.group(3))
+        d = int(matchip4.group(4))
         if a >= 0 and a <= 255 and b>= 0 and b<= 255 and c>= 0 and c <= 255 and d>= 0 and d<= 255:
-            print 'IPv4'
-            continue
-    matchip6 = re.search(r'[]:[]:[]:', str)
+            return 'IPv4'
+    matchip6 = re.search(r'[0-9a-f]{1,4}:[0-9a-f]{1,4}:[0-9a-f]{1,4}:[0-9a-f]{1,4}:[0-9a-f]{1,4}:[0-9a-f]{1,4}:[0-9a-f]{1,4}:[0-9a-f]{1,4}', str)
     if matchip6:
-        print 'IPv6'
-        continue
-    print 'Neither'
+        return 'IPv6'
+    return 'Neither'
+    
+    
+all = []
+for _ in range (input()):
+    all.append(raw_input())
+for item in all:
+    print validate(item)
+    
+    
