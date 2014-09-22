67040b43144ac2837aff01e111930cb0491c429f
kacperharasim <kacper.harasim@gmail.com>
2014-09-22 19:43:13 +0200

    +

--- re/html_links.py
+++ re/html_links.py
@@ -1,4 +1,10 @@
 import re
-searchText = '<p><a href="http://www.quackit.com/html/tutorial/html_links.cfm">Example Link</a></p>'
-match = re.findall(r'<a href="(.+)">.+>(\w+\s*\w+)', searchText, re.I)
-print match[0]
+
+
+for _ in range (input()):
+    searchText = raw_input()
+    match_link = re.findall(r'<a href="(.+)">', searchText, re.I)
+    match_text_field = re.findall(r'>([\w\s.]+)', searchText, re.I)
+    if len(match_link) > 0 and len(match_text_field) > 0:
+        print ("%s,%s" % (match_link[0], match_text_field[0]))
+    
